2812856a0c1b37fdff78620c0f0e858e76f33eb1
RetroGeek46 <divyangpradeep@gmail.com>
2022-07-20 19:59:39 +0530

    Add heroku deployment files

--- setup.py
+++ setup.py
@@ -7,5 +7,8 @@ setup(
     name='GateApproval',
     packages=['GateApproval'],
     include_package_data=True,
-    install_requires=['Flask==2.1.3', 'Flask-Cors==3.0.10', 'jwt==1.3.1', 'pandas==1.4.3', 'requests==2.28.1', 'SQLAlchemy==1.4.39', 'deepface==0.0.75']
+    install_requires=[
+        'Flask==2.1.3', 'Flask-Cors==3.0.10', 'jwt==1.3.1',
+        'pandas==1.4.3', 'requests==2.28.1', 'SQLAlchemy==1.4.39',
+        'deepface==0.0.75', 'gunicorn==20.1.0', 'PyJWT==2.0.1']
 )
